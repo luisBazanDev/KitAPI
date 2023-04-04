45df3809cce52705201d65e4641f499bedb096aa
Luis Bazan <luis@bazan.pe>
2023-04-03 19:58:59 -0500

    First commit

---
+++ src/main/java/pe/bazan/luis/plugins/kitapi/utils/ItemSerialize.java
@@ -0,0 +1,39 @@
+package pe.bazan.luis.plugins.kitapi.utils;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
+public class ItemSerialize {
+    @Nullable
+    public static String serialize(@NotNull ItemStack itemStack) {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+            dataOutput.writeObject(itemStack);
+            dataOutput.close();
+            return Base64.getEncoder().encodeToString(outputStream.toByteArray());
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Nullable
+    public static ItemStack deserialize(@NotNull String data) {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64.getDecoder().decode(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack itemStack = (ItemStack) dataInput.readObject();
+            dataInput.close();
+            return itemStack;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
